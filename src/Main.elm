8e93912fe4fce993d983e4aaff4d0359ab51c1eb
Ian Denhardt <ian@zenhack.net>
2021-11-29 18:27:16 -0500

    Rough UI for submitting tasks

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,10 @@
 module Main exposing (main)
 
+import Accessors
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (disabled, for, name, type_)
+import Html.Events exposing (onClick, onInput)
 import Time
 
 
@@ -11,40 +14,149 @@ type alias Flags =
 
 type alias Model =
     { jobs : List Job
+    , newJob : JobForm
     }
+
+
+type alias JobForm =
+    { title : String
+    , period : String
+    }
+
+
+recordNewJob =
+    Accessors.makeOneToOne .newJob (\c r -> { r | newJob = c r.newJob })
+
+
+recordTitle =
+    Accessors.makeOneToOne .title (\c r -> { r | title = c r.title })
+
+
+recordPeriod =
+    Accessors.makeOneToOne .period (\c r -> { r | period = c r.period })
+
+
+makeJob : JobForm -> Maybe Job
+makeJob jobForm =
+    if jobForm.title == "" then
+        Nothing
+
+    else
+        case String.toInt jobForm.period of
+            Nothing ->
+                Nothing
+
+            Just period ->
+                if period < 1 then
+                    Nothing
+
+                else
+                    Just
+                        { period = period
+                        , title = jobForm.title
+                        , lastDone = Nothing
+                        }
 
 
 type alias Job =
     { title : String
     , period : Int
-    , lastDone : Time.Posix
+    , lastDone : Maybe Time.Posix
     }
 
 
-type alias Msg =
-    ()
+type alias Accessor super sub =
+    Accessors.Relation sub sub sub -> Accessors.Relation super sub sub
+
+
+type Msg
+    = UpdateFormField (Accessor JobForm String) String
+    | NewJob Job
 
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     ( { jobs = []
+      , newJob =
+            { title = ""
+            , period = ""
+            }
       }
     , Cmd.none
     )
 
 
 view : Model -> Browser.Document Msg
-view _ =
+view model =
     { title = "Hello, World!"
     , body =
         [ p [] [ text "Hello, World!" ]
+        , viewJobs model
+        , viewNewJob model.newJob
         ]
     }
 
 
+viewJob : Job -> Html Msg
+viewJob job =
+    div []
+        [ p [] [ text "Title: ", text job.title ]
+        ]
+
+
+viewJobs : Model -> Html Msg
+viewJobs model =
+    ol []
+        (List.map
+            (\job -> li [] [ viewJob job ])
+            model.jobs
+        )
+
+
+viewNewJob : JobForm -> Html Msg
+viewNewJob jobForm =
+    div []
+        [ div []
+            [ label [ for "title" ] [ text "Title: " ]
+            , input
+                [ name "title"
+                , onInput (UpdateFormField recordTitle)
+                ]
+                []
+            ]
+        , div []
+            [ label [ for "period" ] [ text "Period (days): " ]
+            , input
+                [ type_ "number"
+                , name "peroid"
+                , onInput (UpdateFormField recordPeriod)
+                ]
+                []
+            ]
+        , button
+            [ case makeJob jobForm of
+                Nothing ->
+                    disabled True
+
+                Just job ->
+                    onClick (NewJob job)
+            ]
+            [ text "Create" ]
+        ]
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
-update () model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        UpdateFormField accessor value ->
+            ( Accessors.set (recordNewJob << accessor) value model
+            , Cmd.none
+            )
+
+        NewJob job ->
+            ( { model | jobs = job :: model.jobs }
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
